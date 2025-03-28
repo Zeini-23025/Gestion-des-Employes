876ba96deb65b744d99ea93e4ec8083f72b0c6c7
zeini <23025@supnum.mr>
2025-03-28 13:23:16 +0000

    Initial commit of gestion-des-employes project

---
+++ src/main/java/com/zeiny/gestion_des_employes/GestionDesEmployesApplication.java
@@ -0,0 +1,13 @@
+package com.zeiny.gestion_des_employes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDesEmployesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDesEmployesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zeiny/gestion_des_employes/GestionDesEmployesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeiny.gestion_des_employes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDesEmployesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
